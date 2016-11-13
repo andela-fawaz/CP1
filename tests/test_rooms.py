aeb01de07bff7634d14e5ea49c4016f793ee98f0
Fawaz Farid <fawwaz.ali@andela.com>
2016-11-13 17:29:31 +0300

    added setUp method to reduce redundancy

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -1,10 +1,14 @@
 import unittest
 from amity.models.rooms import Room, LivingSpace, Office
 
+
 class TestRooms(unittest.TestCase):
+    def setUp(self):
+        self.office = Office('Krypton')
+        self.ls = LivingSpace('Scala')
 
     def test_office_is_instance_of_room(self):
-        self.assertTrue('Room', type(Office))
+        self.assertTrue(Room, type(self.office))
 
     def test_livingspace_is_instance_of_room(self):
-        self.assertTrue('Room', type(LivingSpace))
+        self.assertTrue(Room, type(self.ls))
