1e18b163b77083d8220dd39552a3317290dd2ea9
Fawaz Farid <fawwaz.ali@andela.com>
2016-11-21 23:17:02 +0300

    Added add person functionality

--- amity/amity.py
+++ amity/amity.py
@@ -1,5 +1,26 @@
 class Amity(object):
-    """
-    This is the amity Class
-    """
-    pass
+    people = []
+    rooms = []
+
+    @staticmethod
+    def add_person(fname, lname, type, allocation):
+        if not isinstance(fname, str) or not isinstance(lname, str):
+            return "Invalid Name!"
+        else:
+            full_names = fname + " " + lname
+            full_names = full_names.upper()
+
+            Amity.people.append({
+                full_names : {
+                    'type' : 'Fellow',
+                    'room_allocated' : 'Scala',
+                },
+            })
+            return "Person Added Succesfully!"
+
+    @staticmethod
+    def create_room(self, *args):
+        pass
+
+    def print_allocations(self):
+        pass
