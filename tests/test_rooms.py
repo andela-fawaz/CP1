7a75bd044e0569af3d5d0fdd64030fb8173c95b7
Fawaz Farid <fawwaz.ali@andela.com>
2016-12-21 00:58:12 +0300

    Tests for rooms

--- tests/test_rooms.py
+++ tests/test_rooms.py
@@ -1,18 +1,18 @@
-# import unittest
-# from amity.models.rooms import Room, LivingSpace, Office
+import unittest
+from amity.models.rooms import Room, LivingSpace, Office
 
 
-# class TestRooms(unittest.TestCase):
-#     def setUp(self):
-#         self.office = Office('Krypton')
-#         self.ls = LivingSpace('Scala')
+class TestRooms(unittest.TestCase):
+    def setUp(self):
+        self.office = Office('Krypton')
+        self.ls = LivingSpace('Scala')
 
-#     def test_office_is_instance_of_room(self):
-#         self.assertTrue(Room, type(self.office))
+    def test_office_is_instance_of_room(self):
+        self.assertTrue(Room, type(self.office))
 
-#     def test_livingspace_is_instance_of_room(self):
-#         self.assertTrue(Room, type(self.ls))
+    def test_livingspace_is_instance_of_room(self):
+        self.assertTrue(Room, type(self.ls))
 
-#     def test_rooms_capacities(self):
-#         self.assertEqual(self.office.capacity, 6)
-#         self.assertEqual(self.ls.capacity, 4)
+    def test_rooms_capacities(self):
+        self.assertEqual(self.office.capacity, 6)
+        self.assertEqual(self.ls.capacity, 4)
