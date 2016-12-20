cb56b2c695cc70565aae9db7eb51f06c101cae87
Fawaz Farid <fawwaz.ali@andela.com>
2016-12-21 00:59:35 +0300

    Implement Sqlalchemy to Models

--- amity/models/people.py
+++ amity/models/people.py
@@ -1,17 +1,47 @@
-class Person(object):
+from dbl import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Enum,
+    ForeignKey,
+)
 
-    def __init__(self, name):
-        self.name = name
+
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    role = Column(String)
+    office_id = Column(Integer, ForeignKey('office.id'))
+    office = relationship("Office", back_populates="people")
+
+    __mapper_args__ = {
+        "polymorphic_on": role,
+        'polymorphic_identity': 'person',
+        'with_polymorphic': '*',
+        }
 
     def get_person_details(self):
         raise NotImplementedError("This is an abstract method")
 
-    def allocate_office(self):
-        pass
+    def __repr__(self):
+        return self.name
 
 
 class Staff(Person):
-    pass
+    __tablename__ = 'staff'
+    id = Column(Integer, ForeignKey('person.id'), primary_key=True, autoincrement=True)
+    __mapper_args__ = {"polymorphic_identity": "Staff"}
+
 
 class Fellow(Person):
-    wants_accomodation = 'N'
+    __tablename__ = 'fellow'
+    id = Column(Integer, ForeignKey('person.id'), primary_key=True, autoincrement=True)
+    __mapper_args__ = {"polymorphic_identity": "Fellow"}
+
+    wants_accomodation = Column(Enum('Y', 'N'))
+
+    livingspace_id = Column(Integer, ForeignKey('livingspaces.id'))
+    livingspace = relationship("LivingSpace", back_populates="people")
--- amity/models/rooms.py
+++ amity/models/rooms.py
@@ -1,38 +1,58 @@
-class Room(object):
+from dbl import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
+
+class Room(object):
     capacity = 0
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
 
     def __init__(self, name):
         self.name = name
-        self.people = []
 
     def __repr__(self):
         return self.name
 
     def get_occupants(self):
+        """
+        Get all Occupants of a Room
+        :returns - A list of Objects(People)
+        """
         return self.people
 
     def add_person(self, person):
         """
         Add person to a room
-        @params Instance of person
+        :params Instance of person
         """
         # Check if room is filled.
         if len(self.people) < self.capacity:
             self.people.append(person)
-            if isinstance(self, LivingSpace):
-                # person.allocate_livingspace
-                pass
-            else:
-                # person.allocate_office
-                pass
+
+    def is_filled(self):
+        """
+        Checks if a room is filled.
+        :returns - Bool(True/False)
+        """
+        people = self.get_occupants()
+        if len(people) == self.capacity:
+            return True
+        return False
 
 
-
-
-class Office(Room):
+class Office(Room, Base):
+    __tablename__ = "office"
     capacity = 6
 
+    people = relationship('Person', back_populates='office')
 
-class LivingSpace(Room):
+
+class LivingSpace(Room, Base):
     capacity = 4
+    __tablename__ = "livingspaces"
+    people = relationship('Fellow', back_populates='livingspace')
